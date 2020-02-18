7f71507cb7e2c6f56bcebd497b43fbc451237725
rolypoly2020 <2667294317@qq.com>
2020-02-18 14:55:39 +0800

    输出方式比对

--- 温度转换.py
+++ 温度转换.py
@@ -3,10 +3,12 @@ x=input("please input a tempture with C/F:\n")
 if x[-1] in ['C','c']:
     c=eval(x[:-1])
     f=c*1.8+32
-    print("转换后的华氏温度%.2f F"%f)
+    ''' print("转换后的华氏温度%.2f F"%f)'''
+    print("转换后的华氏温度{:.2f}F".format(f))
 elif x[-1] in ['F','f']:
     f=eval(x[:-1])
     c=(f-32)/1.8
-    print("转换后的摄氏温度%.2f C"%c)
+    ''' print("转换后的摄氏温度%.2f C"%c)'''
+    print("转换后的摄氏温度{:.2f}C".format(c))
 else:
     print("it is wrong input")
