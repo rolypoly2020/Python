c6ef32821ae1c8d69c897e9763783622183daffe
rolypoly2020 <2667294317@qq.com>
2020-02-18 14:49:20 +0800

    方法2

--- 列表法筛选素数.py
+++ 列表法筛选素数.py
@@ -4,5 +4,9 @@ m=pow(maxnumber,0.5)
 for index,values in enumerate(lst):
     if values>m:
         break
+    '''for value1 in lst[:index:-1]:
+       
+        if value1%values==0:
+            lst.remove(value1)'''
     lst[index+1:]=filter(lambda x:x%values!=0,lst[index+1:])
 print(lst)
