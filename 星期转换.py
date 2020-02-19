3d1ddcc97a7187913c6c1c0be09e1b2fe0cb10fa
rolypoly2020 <2667294317@qq.com>
2020-02-19 17:29:56 +0800

    字符串.py

---
+++ 星期转换.py
@@ -0,0 +1,9 @@
+#weekprint1.py
+weekstr="星期一星期二星期三星期四星期五星期六星期日"
+weeknum=eval(input("请输入1-7的数字:"))
+x=3*(weeknum-1)
+print(weekstr[x:x+3])
+#方法二
+weekstr='一二三四五六七'
+weeknum=eval(input("请输入1-7的数字"))
+print("星期",weekstr[weeknum-1])
